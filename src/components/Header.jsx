f8ab316355f01d3c69f8c1aa1f0ce5b85778ced5
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-12 11:19:52 +0100

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Logo from './Logo'
+export default function Header() {
+    return (
+        <header className=' m-5'>
+            <Logo />
+        </header>
+    )
+}
