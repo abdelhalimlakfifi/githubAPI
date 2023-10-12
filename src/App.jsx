f3422430e7b87ac40ae204d7f26910d9ec2d6b3a
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-12 10:47:29 +0100

    import navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-import Test from './pages/Test';
+import Navbar from './components/Header'
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <Navbar />
 
 
             <Routes>
