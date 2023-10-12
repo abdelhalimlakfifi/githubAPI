7a468ee3469b051a666daed185eda3a92e7654ee
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-12 10:14:49 +0100

    make react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Test from './pages/Test';
 function App() {
     const [count, setCount] = useState(0)
 
@@ -11,6 +11,11 @@ function App() {
             <h1 className="text-3xl font-bold underline">
                 Hello world!
             </h1>
+
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
             
         </>
     )
